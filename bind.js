51effc00e343bda20486d3c03b5e55e12f0c08eb
AkaAny <fkd_any@qq.com>
2021-09-29 13:41:10 +0800

    add: data source

--- bind.js
+++ bind.js
@@ -57,3 +57,15 @@ function NewWhere(query,values){
 function SaveToDB(entityName,x){
     return "stub";
 }
+
+function NewDataSource(dataSourceName,dataSourceType,dsn){
+    return {
+        Name:dataSourceName,
+        Type:dataSourceType,
+        DSN:dsn,
+    }
+}
+
+var DeclareDataSource=function (usage,v){
+    throw "stub";
+}
