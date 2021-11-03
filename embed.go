f2a60b66048a3a7ac93affe5b0332bcff033ed06
AkaAny <fkd_any@qq.com>
2021-11-03 08:53:30 +0800

    update: separate data source code

--- embed.go
+++ embed.go
@@ -7,9 +7,19 @@ import (
 //go:embed bind.js
 var BindJS string
 
+//go:embed data_source.js
+var DataSourceJS string
+
 func GetBindJS() string {
 	if BindJS == "" {
 		panic("invalid bind js")
 	}
 	return BindJS
+}
+
+func GetDataSourceJS() string {
+	if DataSourceJS == "" {
+		panic("invalid datasource js")
+	}
+	return DataSourceJS
 }
